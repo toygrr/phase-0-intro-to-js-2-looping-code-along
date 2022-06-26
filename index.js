bb3dc035e0eb32ec9e0188a1471334c456ccc437
Tyler Montobbio <toygrrr@gmail.com>
2022-06-25 17:31:31 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name, event){
+  let messages = [];
+    for (let i = 0; i < name.length; i++){
+      messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+  }
+  return messages;
+}
+
+function countDown(int){
+  let i = 10;
+  while (i>=0){
+  console.log(i)
+  i--
+  }
+}
