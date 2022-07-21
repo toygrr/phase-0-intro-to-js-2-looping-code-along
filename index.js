e23ff6d6b3615ce14ba2dfcf9f7057137616f4c5
Tyler <toygrrr@gmail.com>
2022-07-21 11:59:07 -0700

    commit tree bd18ea2236d18939e8ed6029e71255114ac83ab6

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-function writeCards(name, event){
-  let messages = [];
-    for (let i = 0; i < name.length; i++){
-      messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts} and added a bow!`);
   }
-  return messages;
+
+  return gifts;
 }
 
-function countDown(int){
-  let i = 10;
-  while (i>=0){
-  console.log(i)
-  i--
+wrapGifts(gifts);
+
+function writeCards(names, event) {
+  let card = [];
+  for (let c = 0; c < names.length; c++) {
+    card.push(`Thank you, ${names[c]}, for the wonderful ${event} gift!`);
   }
+  return card;
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(a) {
+  while (a > 0) {
+    console.log(a);
+    a--;
+  }
+  return console.log(a);
 }
